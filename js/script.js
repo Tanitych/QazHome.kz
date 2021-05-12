ce2c1f7eb894325ee1eaf08e7e5313d3ea0a2abf
Tanita <Tanitych@gmail.com>
2021-05-12 19:41:05 +0600

    range problem

--- js/script.js
+++ js/script.js
@@ -23,3 +23,68 @@ $(document).ready(function () {
         maxTilt: 5,
     })
 })
+
+
+const program_swiper = new Swiper('.program_swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    freeMode: true,
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    initialSlide: 1,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+//tabs
+
+const tabsBtn = document.querySelectorAll(".tabs__nav__btn");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+tabsBtn.forEach(function (item) {
+    item.addEventListener("click", function () {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+
+        if (!currentBtn.classList.contains("active")) {
+            tabsBtn.forEach(function (item) {
+                item.classList.remove("active");
+            });
+
+            tabsItems.forEach(function (item) {
+                item.classList.remove("active");
+            });
+
+            currentBtn.classList.add("active");
+            currentTab.classList.add("active");
+        }
+    });
+});
+
+document.querySelector('.tabs__nav__btn:nth-child(1)').click();
+
+
+//range slider
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+
+
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+
+slider.addEventListener("input", function () {
+    var x = slider.value;
+    var color = 'linear-gradient(90deg, rgb(5, 178, 205)' + x + '%, rgb(199, 199, 204)' + x + '%)';
+    slider.style.background = color;
+})
